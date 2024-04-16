404f57cec7047b2f5557a4b9c1ce8e2e6b9e3e42
Francisco Javier Silva Cadavid <francisco.silva@udea.edu.co>
2024-04-15 23:08:23 -0500

    Update services

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -9,22 +9,22 @@ export class ProductsService {
   private http = inject(HttpClient)
 
   list(){
-    return this.http.get<Product[]>('http://localhost:8080/list-products');
+    return this.http.get<Product[]>('http://frgaria-api-production.up.railway.app/list-products');
   }
 
   get(id: number){
-    return this.http.get<Product>(`http://localhost:8080/${id}`)
+    return this.http.get<Product>(`http://frgaria-api-production.up.railway.app/${id}`)
   }
 
   create(product: Product){
-    return this.http.post<Product>('http://localhost:8080/create-product', product)
+    return this.http.post<Product>('http://frgaria-api-production.up.railway.app/create-product', product)
   }
 
   update(id: number, product: Product){
-    return this.http.put<Product>(`http://localhost:8080/${id}`, product)
+    return this.http.put<Product>(`http://frgaria-api-production.up.railway.app/${id}`, product)
   }
 
   delete(id: number){
-    return this.http.delete(`http://localhost:8080/${id}`)
+    return this.http.delete(`http://frgaria-api-production.up.railway.app/${id}`)
   }
 }
